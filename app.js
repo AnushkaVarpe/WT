18c739550bf6a65bb28b5fe1c3374fb67f6511aa
AnushkaVarpe <anuvarpe021@gmail.com>
2025-03-15 11:09:09 +0530

    docker

--- app.js
+++ app.js
@@ -11,11 +11,8 @@ import pg from 'pg';
 
 dotenv.config();          
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect()
